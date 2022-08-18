6397aa0c94598797dea7cc88e1ef5bd1e543ffc9
zhangyong <zhangyong@uino.com>
2022-08-18 13:57:35 +0800

    feat: 配置按需加载

--- packages/smarty-ui-vite/vite.config.ts
+++ packages/smarty-ui-vite/vite.config.ts
@@ -1,38 +1,38 @@
+/// <reference types="vite/client" />
 /// <reference types="vitest" />
+
 import { defineConfig } from "vite";
+import type { Plugin, UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import Unocss from "./config/unocss";
+
 // https://vitejs.dev/config/
 
-const rollupOptions = {
-  external: ["vue", "vue-router"],
-  output: {
-    assetFileNames: "[name].[ext]",
-    exports: "named",
-    globals: {
-      vue: "Vue",
-    },
-  },
-};
-
-export default defineConfig({
+export const config = {
   plugins: [
-    vue(),
+    vue() as Plugin,
     // 添加JSX插件
-    vueJsx({
-      // options are passed on to @vue/babel-plugin-jsx
-    }),
+    vueJsx() as Plugin,
     // 添加UnoCSS插件
-    Unocss(),
+    Unocss() as Plugin[],
   ],
   // 添加库模式配置
   build: {
-    rollupOptions,
+    rollupOptions: {
+      external: ["vue", "vue-router"],
+      output: {
+        assetFileNames: "[name].[ext]",
+        exports: "named",
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
     cssCodeSplit: true,
     minify: "terser", // boolean | 'terser' | 'esbuild'
     sourcemap: true, // 输出单独 source文件
-    brotliSize: true, // 生成压缩大小报告
+    reportCompressedSize: true, // 生成压缩大小报告
     lib: {
       entry: "./src/entry.ts",
       name: "SmartyUI",
@@ -54,4 +54,5 @@ export default defineConfig({
       web: [/.[tj]sx$/],
     },
   },
-});
+} as UserConfig;
+export default defineConfig(config as UserConfig);
